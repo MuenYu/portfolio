719230bed0c4c0edae2856e7ce12c9989bf055c2
MuenYu <parasomnia97@gmail.com>
2025-03-31 23:14:22 +1000

    change icons

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -10,15 +10,15 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     <svg
       aria-hidden
       className={classes(styles.monogram, className)}
-      width="48"
-      height="29"
-      viewBox="0 0 48 29"
+      width="51"
+      height="43"
+      viewBox="0 0 51 43"
       ref={ref}
       {...props}
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
+          <path d="M0 0L19.3 24.5714V6.55932L51 43H42.8L25.7 24.155L25.5 43L6.4 18.4286V43H0V0Z" />
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
