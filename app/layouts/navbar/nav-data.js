2c6d1f3a1f2c014229c1b94d8d1e275d6f5245f8
MuenYu <parasomnia97@gmail.com>
2025-04-15 23:35:04 +1000

    refactor(social-links): restructure social links configuration and icons
    Refactor the social links configuration in config.json to a nested structure for better organization. Update nav-data.js to dynamically filter and display social links based on the new configuration. Add new SVG icons for social platforms to support the updated links.

--- app/layouts/navbar/nav-data.js
+++ app/layouts/navbar/nav-data.js
@@ -21,18 +21,52 @@ export const navLinks = [
 
 export const socialLinks = [
   {
+    label: 'Blog',
+    url: `${config.social.blog}`,
+    icon: 'home',
+  },
+  {
+    label: 'Github',
+    url: `https://github.com/${config.social.github}`,
+    icon: 'github',
+  },
+  {
+    label: 'Telegram',
+    url: `https://t.me/${config.social.telegram}`,
+    icon: 'telegram',
+  },
+  {
+    label: 'LinkedIn',
+    url: `https://www.linkedin.com/in/${config.social.linkedin}/`,
+    icon: 'linkedin',
+  },
+  {
+    label: 'X',
+    url: `https://x.com/${config.social.x}`,
+    icon: 'x',
+  },
+  {
+    label: 'LeetCode',
+    url: `https://leetcode.com/u/${config.social.leetcode}`,
+    icon: 'leetcode',
+  },
+  {
+    label: 'YouTube',
+    url: `https://www.youtube.com/${config.social.youtube}`,
+    icon: 'youtube',
+  },
+  {
     label: 'Bluesky',
-    url: `https://bsky.app/profile/${config.bluesky}`,
+    url: `https://bsky.app/profile/${config.social.bluesky}`,
     icon: 'bluesky',
   },
   {
     label: 'Figma',
-    url: `https://www.figma.com/${config.figma}`,
+    url: `https://www.figma.com/${config.social.figma}`,
     icon: 'figma',
   },
-  {
-    label: 'Github',
-    url: `https://github.com/${config.github}`,
-    icon: 'github',
-  },
-];
+].filter(x=> {
+  const flag = config.social[x.label.toLowerCase()];
+  // Filter out social links that are not in config.json
+  return flag !== undefined && flag.length > 0;
+});
